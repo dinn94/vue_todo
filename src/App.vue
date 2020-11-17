2c393811d38e2a418d8ce2a0cfe877f5e776f89b
unknown <dinn94>
2020-11-17 11:13:13 +0800

    adding AddTodo functionality

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <Header />
+    <AddTodo v-on:add-todo="addTodo" />
     <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
   </div>
 </template>
@@ -10,12 +11,14 @@
 // import HelloWorld from './components/HelloWorld.vue'
   import Header from './components/layout/Header';
   import Todos from './components/Todos';
+  import AddTodo from './components/AddTodo';
 
 export default {
   name: 'App',
   components: {
     Header,
-    Todos
+    Todos,
+    AddTodo
   },
   data(){
     return {
@@ -43,6 +46,9 @@ export default {
   methods : {
     deleteTodo(id){
       this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo){
+      this.todos = [...this.todos, newTodo];
     }
   }
 }
@@ -59,5 +65,20 @@ export default {
 body{
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+
+.btn{
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+
+.btn:hover {
+  background: #666;
 }
 </style>
