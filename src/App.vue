32888ad759d385aa99ae88f488f47807ea97fb5c
unknown <dinn94>
2020-11-17 12:26:24 +0800

    implementing add todo functionality with backend fake rest api

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
   import Header from './components/layout/Header';
   import Todos from './components/Todos';
   import AddTodo from './components/AddTodo';
+  import axios from 'axios';
 
 export default {
   name: 'App',
@@ -23,23 +24,6 @@ export default {
   data(){
     return {
       todos : [
-        {
-          id : 1,
-          title : 'Todo One',
-          isCompleted : false
-        },
-
-        {
-          id : 2,
-          title : "Todo Two",
-          isCompleted : true
-        },
-
-        {
-          id : 3,
-          title : "Todo Three",
-          isCompleted : false
-        }
       ]
     }
   },
@@ -48,8 +32,20 @@ export default {
       this.todos = this.todos.filter(todo => todo.id !== id);
     },
     addTodo(newTodo){
-      this.todos = [...this.todos, newTodo];
+      const {title, completed} = newTodo;
+
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
     }
+  },
+  created(){
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    .then(res => this.todos = res.data)
+    .catch(err => console.log(err));
   }
 }
 </script>
